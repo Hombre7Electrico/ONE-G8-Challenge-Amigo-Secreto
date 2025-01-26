0cf3af2ad28167ae85594f4c4998851443d31f4e
Hombre7Electrico <leandro.albornoz1492@gmail.com>
2025-01-26 10:59:10 -0300

    26-1-2025

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*En este desafío, desarrollarás una aplicación que permita a los usuarios ingresar nombres de amigos en una lista para luego realizar 
+un sorteo aleatorio y determinar quién es el "amigo secreto".
+El usuario deberá agregar nombres mediante un campo de texto y un botón "Adicionar". Los nombres ingresados se mostrarán en una lista 
+visible en la página, y al finalizar, un botón "Sortear Amigo" seleccionará uno de los nombres de forma aleatoria, mostrando el resultado
+en pantalla.
+
+Fucionalidades:
+1- Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en 
+"Adicionar".
+2- Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+3- Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+4- Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la 
+página.*/
+
+//variables
 let amigos = [];
+
+//programa principal
+
+//funciones
+function agregarAmigo(){
+amigos.push = Document.getElementById('amigo');
+borrarLabel();
+
+}
+
+function sortearAmigo(){
+    
+}
+
+function borrarLabel(){
+    
+}
